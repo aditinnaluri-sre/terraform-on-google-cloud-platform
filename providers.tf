5a1691f12fe1885bf00de220a228ff1c0922a33e
Adi Tinnaluri <tans.trainings@gmail.com>
2026-06-03 15:49:36 +0100

    update provide file

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "terraform-438115"
-  region  = "europe-north1"
-  zone    = "europe-north1-a"
+  project = "terraform-gcp-498312"
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
 }
