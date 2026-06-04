6e7a11f8a80b5b008827d0bd773c8e37be6a8137
Adi Tinnaluri <Adi.Tinnaluri@virginmedia.ie>
2026-06-04 14:17:11 +0100

    main.tf updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "buk-dev-tans-trainings" {
-  name          = "var.bucket_name"
-  location      = "var.region"
-  project = "var.project_id"
+  name     = "var.bucket_name"
+  location = "var.region"
+  project  = "var.project_id"
 
 
   force_destroy = "var.force_destory"
@@ -10,9 +10,9 @@ resource "google_storage_bucket" "buk-dev-tans-trainings" {
     condition { num_newer_versions = "var.num_newer_versions" }
     action { type = "Delete" }
   }
-  
+
   labels = {
-    env = var.environment
+    env        = var.environment
     managed-by = "terraform"
   }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,7 @@
 project_id         = "terraform-gcp-498312"
 region             = "europe-west2"
 environment        = "dev"
-bucket_name        = "tf-state-$project_id"   # must be globally unique
+bucket_name        = "tf-state-$project_id" # must be globally unique
 force_destroy      = false
 versioning_enabled = true
 num_newer_versions = 5
