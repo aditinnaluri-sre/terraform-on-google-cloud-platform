d8032fbf8eb42dc5ffc57c5671a9cbc8a5050ead
Adi Tinnaluri <Adi.Tinnaluri@virginmedia.ie>
2026-06-04 13:37:52 +0100

    code modified

--- main.tf
+++ main.tf
@@ -1,38 +1,23 @@
-resource "google_compute_subnetwork" "vpc-network" {
-  name          = "vpc-subnetwork"
-  ip_cidr_range = "172.30.2.0/24"
-  region        = "europe-north1"
-  network       = google_compute_network.custom-test.id
-  }
+resource "google_storage_bucket" "buk-dev-tans" {
+  name          = "auto-expiring-bucket"
+  location      = "EU"
+  force_destroy = true
 
-resource "google_compute_network" "vpc-network" {
-  name                    = "vpc-network"
-  auto_create_subnetworks = false
-}
-
-
-data "google_compute_image" "ubuntu" {
-  most_recent = true
-  project     = "ubuntu-os-cloud" 
-  family      = "ubuntu-2204-lts"
-}
-
-
-
-resource "google_compute_instance" "gcp-client-vm" {
-  name         = "gcp-client-vm"
-  machine_type = "e2-standard-2"
-  
-  boot_disk {
-    initialize_params {
-      image = data.google_compute_image.ubuntu.self_link
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
     }
   }
-  network_interface {
-   subnetwork = "vpc-subnetwork"
-   access_config {
-      # Leave empty for dynamic public IP
-    }
-  }  
 
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
