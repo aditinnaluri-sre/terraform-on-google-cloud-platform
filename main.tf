3acf268ffbdddb76c48e9601c305fcef697b65e9
Adi Tinnaluri <Adi.Tinnaluri@virginmedia.ie>
2026-06-04 14:06:14 +0100

    main.tf updated

--- main.tf
+++ main.tf
@@ -1,23 +1,19 @@
-resource "google_storage_bucket" "buk-dev-tans" {
-  name          = "auto-expiring-bucket"
-  location      = "EU"
-  force_destroy = true
+resource "google_storage_bucket" "buk-dev-tans-trainings" {
+  name          = "var.bucket_name"
+  location      = "var.region"
+  project = "var.project_id"
+
+
+  force_destroy = "var.force_destory"
 
   lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
+    condition { num_newer_versions = "var.num_newer_versions" }
+    action { type = "Delete" }
+  }
+  
+  labels = {
+    env = var.environment
+    managed-by = "terraform"
   }
 
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
 }
